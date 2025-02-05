4ef4e93b492c273fcf980756f1124738dbb6e9fc
Deep5Varshney <137312194+Deep5Varshney@users.noreply.github.com>
2025-02-06 00:04:01 +0530

    stored sample data in db

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ const db_link = 'mongodb+srv://admin:pxFOADAWKdmcJ16U@cluster0.d3d3w.mongodb.net
 //     console.log("Not connected = Error => ", e);
 //    });
 
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
+
 mongoose.connect(db_link)
     .then(() => {
         console.log("db connected");
@@ -23,6 +27,38 @@ mongoose.connect(db_link)
        // console.error("Topology description:", err.reason);
     });
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:8
+    },
+    confirmPassword:{
+        type:String,
+        required:true,
+        minLength:8
+    }
 });
+
+const userModel = mongoose.model('userModel', userSchema);
+
+(async function createUser(){
+    let user ={
+        name :"Deepshikha",
+        email: "xyza@gmail.com",
+        password : "123456789",
+        confirmPassword: "123456789"
+    };
+
+    let data = await userModel.create(user);
+    console.log(data);
+})();
